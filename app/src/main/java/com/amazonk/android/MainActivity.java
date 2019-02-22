ffa43ea10448a0a41f2164b843669e3d0960b153
diwangs <27280516+diwangs@users.noreply.github.com>
2019-02-22 13:11:43 +0700

    Succesfully read MiFare card data

--- app/src/main/java/com/amazonk/android/MainActivity.java
+++ app/src/main/java/com/amazonk/android/MainActivity.java
@@ -1,9 +1,14 @@
 package com.amazonk.android;
 
 import android.app.ActivityManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcF;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -14,6 +19,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -22,6 +28,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.math.BigInteger;
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
@@ -29,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle drawerToggle;
     private DrawerLayout mDrawer;
     private Intent mServiceIntent;
+    private TextView mTextView;
+
+
+    // NFC
+    private NfcAdapter mNfcAdapter;
+    private PendingIntent mPendingIntent;
+    private IntentFilter[] mIntentFilters;
+    private String[][] mNFCTechLists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
         if (!isMyServiceRunning(LockService.class)) {
             startService(mServiceIntent);
         }
+
+        // TextView
+        mTextView = (TextView)findViewById(R.id.textView);
 
         // Set a Toolbar to replace the ActionBar.
         toolbar = findViewById(R.id.toolbar);
@@ -77,6 +96,46 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+
+        // NFC setup
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter != null)
+            mTextView.setText("Read an NFC tag");
+        else
+            mTextView.setText("This phone is not NFC enabled.");
+
+        // create an intent with tag data and deliver to this activity
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        // set an intent filter for all MIME data
+        IntentFilter tagIntent = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+
+        try {
+            mIntentFilters = new IntentFilter[] { tagIntent };
+        } catch (Exception e) {
+            Log.e("TagDispatch", e.toString());
+        }
+
+        mNFCTechLists = new String[][] { new String[] { NfcF.class.getName() } };
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mNfcAdapter != null)
+            mNfcAdapter.enableForegroundDispatch(this, mPendingIntent, mIntentFilters, mNFCTechLists);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mNfcAdapter != null)
+            mNfcAdapter.disableForegroundDispatch(this);
     }
 
     @Override
@@ -142,4 +201,19 @@ public class MainActivity extends AppCompatActivity {
         Log.i ("isMyServiceRunning?", false+"");
         return false;
     }
+
+    // NFC
+    @Override
+    public void onNewIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            mTextView.setText(bin2hex(tag.getId())); // id-nya RFID
+        }
+    }
+
+    static String bin2hex(byte[] data) {
+        return String.format("%0" + (data.length * 2) + "X", new BigInteger(1,data));
+    }
+
 }
--- app/src/main/java/com/amazonk/android/SplashActivity.java
+++ app/src/main/java/com/amazonk/android/SplashActivity.java
@@ -1,14 +1,10 @@
 package com.amazonk.android;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
 
