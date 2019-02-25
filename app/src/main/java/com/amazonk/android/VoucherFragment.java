6ac28e7a16d53dd97bdc3fe4176f9c98f664b91c
Muhammad Abdullah Munir <13516074@std.stei.itb.ac.id>
2019-02-25 10:41:17 +0700

    Add voucher listener

--- app/src/main/java/com/amazonk/android/VoucherFragment.java
+++ app/src/main/java/com/amazonk/android/VoucherFragment.java
@@ -13,10 +13,14 @@ import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,6 +35,7 @@ import java.util.Map;
  * create an instance of this fragment.
  */
 public class VoucherFragment extends Fragment {
+    private static final String TAG = VoucherFragment.class.getSimpleName();
     private ListView mVoucherListView;
     private DocumentReference mVouchersDoc;
     private Map<String, Object> voucherMap;
@@ -58,28 +63,40 @@ public class VoucherFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_voucher, container, false);
 
-        mVouchersDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//        mVouchersDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    DocumentSnapshot document = task.getResult();
+//                    if (document.exists()) {
+//                        Log.w("Firehose", "DocumentSnapshot data: " + document.getData());
+//                        voucherKeyArray = document.getData().keySet().toArray(new String[] {});
+////                        Log.d("Firehose", "" + voucherMap.keySet().toArray(new String[] {})[0]);
+////                        voucherObjectArray = voucherMap.values().toArray();
+////                        for (Object v : voucherObjectArray) {
+////                            voucherArrayArrayList.add((String[]) v);
+////                        }
+////                        for (String[] v : voucherArrayArrayList) {
+////                            voucherCodeArrayList.add(v[0]);
+////                        }
+//                    } else {
+//                        Log.w("Firehose", "No such document");
+//                    }
+//                } else {
+//                    Log.w("Firehose", "get failed with ", task.getException());
+//                }
+//            }
+//        });
+
+        mVouchersDoc.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d("Firehose", "DocumentSnapshot data: " + document.getData());
-                        voucherKeyArray = document.getData().keySet().toArray(new String[] {});
-//                        Log.d("Firehose", "" + voucherMap.keySet().toArray(new String[] {})[0]);
-//                        voucherObjectArray = voucherMap.values().toArray();
-//                        for (Object v : voucherObjectArray) {
-//                            voucherArrayArrayList.add((String[]) v);
-//                        }
-//                        for (String[] v : voucherArrayArrayList) {
-//                            voucherCodeArrayList.add(v[0]);
-//                        }
-                    } else {
-                        Log.d("Firehose", "No such document");
-                    }
-                } else {
-                    Log.d("Firehose", "get failed with ", task.getException());
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "listen:error", e);
+                    return;
                 }
+                Log.w(TAG, "new data : " + documentSnapshot.getData());
             }
         });
 
