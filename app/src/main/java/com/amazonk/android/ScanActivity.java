15ab505722ebf27621276183d2ba7903bd6191f4
diwangs <27280516+diwangs@users.noreply.github.com>
2019-02-25 16:00:21 +0700

    add scan

--- app/src/main/java/com/amazonk/android/ScanActivity.java
+++ app/src/main/java/com/amazonk/android/ScanActivity.java
@@ -11,7 +11,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class ScanActivity extends AppCompatActivity {
 
@@ -77,6 +84,35 @@ public class ScanActivity extends AppCompatActivity {
         if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
             Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             mTextView.setText(bin2hex(tag.getId())); // id-nya RFID
+            String idBarang = bin2hex(tag.getId());
+
+            try {
+                URL url = new URL("https://amazonk-shelf-manager.herokuapp.com/open-shelf");
+                HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                http.setRequestMethod("POST");
+                http.setRequestProperty("Content-Type", "application/json");
+                http.setRequestProperty("Accept", "application/json");
+                http.setDoOutput(true);
+
+                String jsonPayload = "{\"idBarang\":\"" + idBarang + "\"}";
+                byte[] out = jsonPayload.getBytes(StandardCharsets.UTF_8);
+                int length = out.length;
+
+                http.setFixedLengthStreamingMode(length);
+                http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                http.connect();
+
+                OutputStream os = http.getOutputStream();
+                os.write(out);
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) Log.d("Scan", inputLine);
+                in.close();
+                os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
