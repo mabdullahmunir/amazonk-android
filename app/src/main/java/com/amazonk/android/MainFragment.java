b6bbc85971ec8b79cb0ab2aef03f2736486d91bd
Muhammad Abdullah Munir <13516074@std.stei.itb.ac.id>
2019-02-25 15:48:30 +0700

    Fix calculating total price

--- app/src/main/java/com/amazonk/android/MainFragment.java
+++ app/src/main/java/com/amazonk/android/MainFragment.java
@@ -10,8 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.amazonk.android.model.Cart;
+import com.amazonk.android.model.Products;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -26,12 +30,14 @@ public class MainFragment extends Fragment {
 
     private static final String TAG = MainFragment.class.getSimpleName();
     private Button mScanButton;
+    private TextView mHargaView;
 
     //Recycler
     private RecyclerView mRecyclerView;
     private ProductAdapter mAdapter;
     final private LinkedList<Cart.Product> mProductList = new LinkedList<>();
     private DocumentReference mCartDoc;
+    private int total;
 
     public MainFragment() {
         // Required empty public constructor
@@ -47,16 +53,19 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, null);
 
+        String userMail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        mHargaView = view.findViewById(R.id.harga_view);
         mScanButton = view.findViewById(R.id.scan_button);
         mScanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mAdapter.notifyDataSetChanged();
                 Intent intent = new Intent(getContext(), ScanActivity.class);
                 startActivity(intent);
             }
         });
 
-        mCartDoc = FirebaseFirestore.getInstance().collection("cart").document("hehe");
+        mCartDoc = FirebaseFirestore.getInstance().collection("cart").document(userMail);
         mCartDoc.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
@@ -64,10 +73,37 @@ public class MainFragment extends Fragment {
                     Log.w(TAG, "listen:error", e);
                     return;
                 }
-                Cart cart = documentSnapshot.toObject(Cart.class);
-                mProductList.clear();
-                mProductList.addAll(cart.getProductList());
-                mAdapter.notifyDataSetChanged();
+
+                total = 0;
+                Cart cart = null;
+
+                try {
+                    cart = documentSnapshot.toObject(Cart.class);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+
+                if (cart != null) {
+                    mProductList.clear();
+                    for (final Cart.Product product : cart.getProductList()) {
+                        FirebaseFirestore
+                                .getInstance()
+                                .collection("products")
+                                .document(product.getIdProduk())
+                                .get()
+                                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                        Products products = documentSnapshot.toObject(Products.class);
+                                        product.setNamaProduk(products.getNamaProduk());
+                                        mProductList.add(product);
+                                        total += products.getHarga() * product.getJumlah();
+                                        mAdapter.notifyDataSetChanged();
+                                        mHargaView.setText("Rp. " + total);
+                                    }
+                                });
+                    }
+                 }
             }
         });
 
